f3f33e3d2544fa16d0f54a0bc285898dadf66b44
Nick Midmore <nmidmore@gmail.com>
2024-11-06 10:10:16 +0000

    add Program.cs, Task00, Task01

--- CSTypes/Program.cs
+++ CSTypes/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace CSTypes
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Task00.Run();
         }
     }
 }
---
+++ CSTypes/Task01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSTypes
+{
+    public class Task01
+    {
+        public static void Run()
+        {
+            float myFloat = 3.80f;
+            string myString = "h";
+            double myDouble = 2.5;
+
+            
+        }
+    }
+}
